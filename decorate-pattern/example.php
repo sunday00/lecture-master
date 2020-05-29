2307fb96d151f051338444e8cbfc0185174d2410
sunday00 <kind65505256@gmail.com>
2020-05-29 18:32:21 +0900

    nested pattern

--- decorate-pattern/example.php
+++ decorate-pattern/example.php
@@ -1,24 +1,45 @@
 <?php
 
-class BasicInspection {
+interface CarService {
+    public function getCost();
+}
+
+class BasicInspection implements CarService {
     public function getCost()
     {
         return 10;
     }
 }
 
-class BasicInspectionAndOilChange {
+class OilChange implements CarService {
+
+    protected $carService;
+
+    public function __construct(CarService $carService) {
+        $this->carService = $carService;
+    }
+
     public function getCost()
     {
-        return 10 + 10;
+        return $this->carService->getCost() + 13;
     }
 }
 
-class BasicInspectionAndOilChangeAndTireRotation {
+class TireRotation implements CarService {
+    protected $carService;
+
+    public function __construct(CarService $carService) {
+        $this->carService = $carService;
+    }
+
     public function getCost()
     {
-        return 10 + 10 + 18;
+        return $this->carService->getCost() + 7;
     }
 }
 
-echo (new BasicInspectionAndOilChangeAndTireRotation())->getCost();
+echo (new TireRotation(new BasicInspection ))->getCost();
+echo "\n";
+echo (new TireRotation(new OilChange( new BasicInspection )))->getCost();
+echo "\n";
+echo (new OilChange(new TireRotation( new BasicInspection )))->getCost();
