61caaa0c7fad56c22f249a055cda5edff8f173c2
sunday00 <kind65505256@gmail.com>
2020-06-02 19:30:33 +0900

    difficult to understand specific

--- specific/src/CustomerRepository.php
+++ specific/src/CustomerRepository.php
@@ -6,11 +6,16 @@ class CustomerRepository
 
     public function bySpecification($customerSpecification)
     {
-        $matches = [];
-        foreach ( $this->all() as $customer ) {
-            if( $customerSpecification->isSatisfiedBy($customer) ) $matches[] = $customer ;
-        }
-        return $matches;
+        // $matches = [];
+        // foreach ( $this->all() as $customer ) {
+        //     if( $customerSpecification->isSatisfiedBy($customer) ) $matches[] = $customer ;
+        // }
+        // return $matches;
+
+        $customers = Customer::query();
+        $customers = $customerSpecification->asScope($customers);
+
+        return $customers->get();
     }
 
     public function all()
