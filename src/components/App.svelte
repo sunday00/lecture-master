a5a149beca7f2cc7a54670cc85d20eefff705843
sunday00 <kind65505256@gmail.com>
2020-06-18 16:49:29 +0900

    static value

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,9 @@
 <h1>Hello {name}!</h1>
+<img {src} alt="adorable avatar" style="border-radius:50%;">
 
 <script>
   let name = 'banana';
+  let src = 'https://api.adorable.io/avatars/100/abott@adorable.io.png';
 </script>
 
 <style>
