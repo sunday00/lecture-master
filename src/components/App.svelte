a3721a612826eac543b8653b50ffa73292052ea1
sunday00 <kind65505256@gmail.com>
2020-06-18 17:47:52 +0900

    $ means wtach rferenced value but even more strong

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,9 +1,24 @@
-<h1>Hello {name}!</h1>
-<img {src} alt="adorable avatar" style="border-radius:50%;">
-<Child></Child>
-{someTag}
-{@html someTag}
+<div>
+  <h1>Hello {name}!</h1>
+  <img {src} alt="adorable avatar" style="border-radius:50%;">
+  <Child></Child>
+  {someTag}
+  {@html someTag}
+</div>
+<div>
+  <button on:click={toggleBtnColor} class="{btnColor}">{btnColor}</button>
 
+  <br>
+
+  <button on:click={increaseRef}>ref++</button>
+  {ref} : {double}
+
+  <br>
+
+  <button on:click={increaseRef2}>ref++</button>
+  {ref2} : {double2}
+
+</div>
 
 <script>
   let name = 'banana';
@@ -13,8 +28,38 @@
 
   let someTag = "<h3>This is html tag</h3>";
 
+  let btnColor = 'red';
+  function toggleBtnColor () {
+    btnColor = btnColor == 'red' ? 'blue' : 'red';
+  }
+
+  let ref = 0;
+  $: double = ref * 2;
+  function increaseRef () {
+    ref++;
+  }
+
+  let ref2 = 0;
+  let double2 = ref2 * 2;
+  function increaseRef2 () {
+    ref2++;
+    double2 = ref2 * 2;
+  }
+  
+  $: console.log(`btn color = ${btnColor}`);
+
+  $: if ( ref2 % 2 ) {
+    console.log(ref2);
+  }
+
 </script>
 
-<style>
+<style lang="sass">
+  button.red {
+    background-color: tomato;
+  }
 
+  button.blue {
+    background-color: cornflowerblue;
+  }
 </style>
