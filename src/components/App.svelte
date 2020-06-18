1a6533e9744854bfe836541dc2c0f6bfe44e06a6
sunday00 <kind65505256@gmail.com>
2020-06-18 16:51:30 +0900

    nested component

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,9 +1,12 @@
 <h1>Hello {name}!</h1>
 <img {src} alt="adorable avatar" style="border-radius:50%;">
+<Child></Child>
 
 <script>
   let name = 'banana';
   let src = 'https://api.adorable.io/avatars/100/abott@adorable.io.png';
+
+  import Child from './Child'
 </script>
 
 <style>
