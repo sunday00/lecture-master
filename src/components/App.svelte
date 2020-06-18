0c9e260f6f6a87311a25017a66a673188b6dd8a6
sunday00 <kind65505256@gmail.com>
2020-06-18 16:57:03 +0900

    html without senitize

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,12 +1,18 @@
 <h1>Hello {name}!</h1>
 <img {src} alt="adorable avatar" style="border-radius:50%;">
 <Child></Child>
+{someTag}
+{@html someTag}
+
 
 <script>
   let name = 'banana';
   let src = 'https://api.adorable.io/avatars/100/abott@adorable.io.png';
 
   import Child from './Child'
+
+  let someTag = "<h3>This is html tag</h3>";
+
 </script>
 
 <style>
