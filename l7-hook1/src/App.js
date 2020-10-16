47885dfbbcf922473d8bc0b86b4a988723a23959
sunday00 <kind65505256@gmail.com>
2020-10-16 19:43:17 +0900

    hook

---
+++ l7-hook1/src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  const [a, setA] = useState(0);
+
+  const onClick = () => {
+    /*
+    setA( a+1 );
+    setA( a+1 );
+    */ // this is not worked twice.
+    // react run async.
+
+    setA((a) => a + 1);
+    setA((a) => a + 1);
+
+    console.log('click');
+  }
+
+  return (
+    <div className="App">
+      <h2>{a}</h2>
+      <button onClick={onClick}>click</button>
+    </div>
+  );
+}
+
+export default App;
