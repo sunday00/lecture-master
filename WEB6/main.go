a30b7246e8bdf47fc72cc88f65193c162a2dde78
sunday00 <kind65505256@gmail.com>
2020-12-03 18:13:39 +0900

    some packages

--- WEB6/main.go
+++ WEB6/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"html/template"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/pat"
 	"github.com/unrolled/render"
+	"github.com/urfave/negroni"
 )
 
 var rd *render.Render
@@ -22,28 +21,37 @@ type User struct {
 func getUserInfoHandler(w http.ResponseWriter, r *http.Request) {
 	user := User{Name: "sunday00", Email: "sunday_0@hotmail.com"}
 
-	w.Header().Add("Content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	data, _ := json.Marshal(user)
-	fmt.Fprint(w, string(data))
+	// w.Header().Add("Content-type", "application/json")
+	// w.WriteHeader(http.StatusOK)
+	// data, _ := json.Marshal(user)
+	// fmt.Fprint(w, string(data))
+	rd.JSON(w, http.StatusOK, user)
 }
 
 func postUserHandler(w http.ResponseWriter, r *http.Request) {
 	user := new(User)
-	err := json.NewDecoder(r.Body).Decode(user)
+	// err := json.NewDecoder(r.Body).Decode(user)
 
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusBadRequest)
+	// 	fmt.Fprint(w, err)
+	// 	return
+	// }
+
+	// user.CreatedAt = time.Now()
+
+	// w.Header().Add("Content-type", "application/json")
+	// w.WriteHeader(http.StatusOK)
+	// data, _ := json.Marshal(user)
+	// fmt.Fprint(w, string(data))
+
+	err := json.NewDecoder(r.Body).Decode(user)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err)
+		rd.Text(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	user.CreatedAt = time.Now()
-
-	w.Header().Add("Content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	data, _ := json.Marshal(user)
-	fmt.Fprint(w, string(data))
+	rd.JSON(w, http.StatusOK, user)
 }
 
 func getHelloHandler(w http.ResponseWriter, r *http.Request) {
@@ -54,12 +62,20 @@ func getHelloHandler(w http.ResponseWriter, r *http.Request) {
 	// data, _ := json.Marshal(user)
 	// fmt.Fprint(w, string(data))
 
-	tmpl, _ := template.New("hello").ParseFiles("templates/hello.tmpl")
-	tmpl.ExecuteTemplate(w, "hello.tmpl", user)
+	// tmpl, _ := template.New("hello").ParseFiles("templates/hello.tmpl")
+	// tmpl.ExecuteTemplate(w, "hello.tmpl", user)
+
+	// rd.HTML(w, http.StatusOK, "hello", user)
+	rd.HTML(w, http.StatusOK, "body", user)
 }
 
 func main() {
-	rd = render.New()
+	// rd = render.New()
+	rd = render.New(render.Options{
+		Directory:  "templates",
+		Extensions: []string{".html", ".tmpl"},
+		Layout:     "hello",
+	})
 
 	// mux := http.NewServeMux()
 	mux := pat.New()
@@ -71,5 +87,8 @@ func main() {
 
 	mux.Get("/hello", getHelloHandler)
 
-	http.ListenAndServe(":3001", mux)
+	n := negroni.Classic()
+	n.UseHandler(mux)
+
+	http.ListenAndServe(":3001", n)
 }
