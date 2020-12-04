ccd3cd7ef32f1c940a7a1e09c69516041ca1ec3f
sunday00 <kind65505256@gmail.com>
2020-12-04 21:03:18 +0900

    immer

--- l010-react-and-redux/src/modules/todos.js
+++ l010-react-and-redux/src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
@@ -73,21 +74,38 @@ const initialState = {
 
 const todos = handleActions(
   {
-    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input: input }),
-    [INSERT]: (state, { payload: todo }) => ({
-      ...state,
-      todos: state.todos.concat(todo),
-    }),
-    [TOGGLE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo,
-      ),
-    }),
-    [REMOVE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.filter((todo) => todo.id !== id),
-    }),
+    // [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input: input }),
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, (draft) => {
+        draft.input = input;
+      }),
+    // [INSERT]: (state, { payload: todo }) => ({
+    //   ...state,
+    //   todos: state.todos.concat(todo),
+    // }),
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, (draft) => {
+        draft.todos.push(todo);
+      }),
+    // [TOGGLE]: (state, { payload: id }) => ({
+    //   ...state,
+    //   todos: state.todos.map((todo) =>
+    //     todo.id === id ? { ...todo, done: !todo.done } : todo,
+    //   ),
+    // }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const todo = draft.todos.find((todo) => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    // [REMOVE]: (state, { payload: id }) => ({
+    //   ...state,
+    //   todos: state.todos.filter((todo) => todo.id !== id),
+    // }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        draft.todos = draft.todos.filter((todo) => todo.id !== id);
+      }),
   },
   initialState,
 );
