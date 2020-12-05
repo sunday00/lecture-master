a926c230d42a4bc3125cfeaaac087eddacfd2c38
sunday00 <kind65505256@gmail.com>
2020-12-05 14:33:16 +0900

    chat

--- WEB7/main.go
+++ WEB7/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/antage/eventsource"
 	"github.com/gorilla/pat"
-	"github.com/sunday00/go-console"
 	"github.com/urfave/negroni"
 )
 
@@ -12,15 +15,55 @@ func postMessageHandler(w http.ResponseWriter, r *http.Request) {
 	msg := r.FormValue("msg")
 	name := r.FormValue("name")
 
-	console.PrintColoredLn(msg, console.Info)
-	console.PrintColoredLn(name, console.Success)
+	sendMessgage(name, msg)
+}
+
+func addUserHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("name")
+	sendMessgage(username, " is Entered this room...")
+}
+
+func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("name")
+	sendMessgage(username, " is left this room...")
+}
+
+//Message ...
+type Message struct {
+	Name string `json:"name"`
+	Msg  string `json:"msg"`
+}
+
+var msgCh chan Message
+
+func sendMessgage(name, msg string) {
+	msgCh <- Message{name, msg}
+}
+
+func processMsgCh(es eventsource.EventSource) {
+	for msg := range msgCh {
+		data, _ := json.Marshal(msg)
+		es.SendEventMessage(string(data), "", strconv.Itoa(time.Now().Nanosecond()))
+	}
 }
 
 func main() {
+	msgCh = make(chan Message)
+
+	es := eventsource.New(nil, nil)
+	defer es.Close()
+
+	go processMsgCh(es)
 
 	mux := pat.New()
 
 	mux.Post("/messages", postMessageHandler)
+
+	mux.Handle("/stream", es)
+
+	mux.Post("/users", addUserHandler)
+
+	mux.Delete("/users", deleteUserHandler)
 
 	n := negroni.Classic()
 	n.UseHandler(mux)
--- WEB7/public/chat.js
+++ WEB7/public/chat.js
@@ -1,7 +1,3 @@
-function isBlank (string){
-    return string == null || string.trim() === "";
-}
-
 $(function(){
     if(!window.EventSource){
         console.error("No event source")
@@ -10,6 +6,19 @@ $(function(){
 
     let $chatLog = $('#chat-log');
     let $chatMsg = $('#chat-msg');
+
+    function isBlank (string){
+        return string == null || string.trim() === "";
+    }
+    
+    function addMessage (data) {
+        let text;
+        if (!isBlank(data.name)){
+            text = `<p><strong>${data.name} : </strong>${data.msg}</p>`;
+        }
+        
+        $chatLog.append(`<div>${text}</div>`);
+    }
 
     let username;
     while(isBlank(username)) {
@@ -32,4 +41,22 @@ $(function(){
         $chatMsg.focus();
     });
 
+    let es = new EventSource('/stream');
+    es.onopen = function(e){
+        $.post('/users', {
+            name: username
+        });
+    }
+    es.onmessage = function(e){
+        let msg = JSON.parse(e.data);
+        addMessage(msg)
+    }
+
+    window.onbeforeunload = function () {
+        $.ajax({
+            url: `/users?name=${username}`,
+            type: 'DELETE'
+        });
+        es.close();
+    }
 });
