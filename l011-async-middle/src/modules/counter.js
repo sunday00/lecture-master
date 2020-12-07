0ce7b1d9831eab2500aeb3adc778e53e43a698b2
sunday00 <kind65505256@gmail.com>
2020-12-07 21:40:13 +0900

    saga apply

--- l011-async-middle/src/modules/counter.js
+++ l011-async-middle/src/modules/counter.js
@@ -1,21 +1,39 @@
 import { createAction, handleActions } from 'redux-actions';
+import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
+const INCREASE_ASYNC = 'counter/INCREASE_ASYNC';
+const DECREASE_ASYNC = 'counter/DECREASE.ASYNC';
 
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
+export const increase_async = createAction(INCREASE_ASYNC, () => undefined);
+export const decrease_async = createAction(DECREASE_ASYNC, () => undefined);
 
-export const increase_async = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(increase());
-  }, 1000);
-};
-export const decrease_async = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(decrease());
-  }, 1000);
-};
+// export const increase_async = () => (dispatch) => {
+//   setTimeout(() => {
+//     dispatch(increase());
+//   }, 1000);
+// };
+// export const decrease_async = () => (dispatch) => {
+//   setTimeout(() => {
+//     dispatch(decrease());
+//   }, 1000);
+// };
+function* increaseSaga() {
+  yield delay(1000);
+  yield put(increase());
+}
+function* decreaseSaga() {
+  yield delay(1000);
+  yield put(decrease());
+}
+
+export function* counterSaga() {
+  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  yield takeLatest(DECREASE_ASYNC, decreaseSaga);
+}
 
 const initialState = 0;
 
--- l011-async-middle/src/modules/index.js
+++ l011-async-middle/src/modules/index.js
@@ -1,16 +1,27 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
+import { all } from 'redux-saga/effects';
+import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import counter from './counter';
+import counter, { counterSaga } from './counter';
 import sample from './sample';
 import loading from './loading';
 import loggerMiddleware from '../lib/loggerMiddleware';
 import ReduxThunk from 'redux-thunk';
 
 const rootReducer = combineReducers({ counter, sample, loading });
+const sagaMiddleware = createSagaMiddleware();
+
+export function* rootSaga() {
+  yield all([counterSaga()]);
+}
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(loggerMiddleware, ReduxThunk)),
+  composeWithDevTools(
+    applyMiddleware(loggerMiddleware, ReduxThunk, sagaMiddleware),
+  ),
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
