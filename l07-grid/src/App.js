11e134cf698c78549423f7e823756269eb72996c
sunday00 <kind65505256@gmail.com>
2020-12-31 18:59:06 +0900

    grid 1

---
+++ l07-grid/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Section from './components/Section';
+import Section2 from './components/Section2';
+
+function App() {
+  return (
+    <div className="App">
+      <Section></Section>
+      <Section2></Section2>
+    </div>
+  );
+}
+
+export default App;
---
+++ l07-grid/src/components/Section.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const Section = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs12</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>xs6</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>xs4</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>xs4</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>xs4</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Section;
