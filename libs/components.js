a25aadb9bebda9669abc5281da5a196b15869e4a
sunday00 <kind65505256@gmail.com>
2021-03-18 00:01:08 +0900

    pannel pop

--- libs/components.js
+++ libs/components.js
@@ -46,25 +46,35 @@ class Box {
 class Panel {
     index;
     size = 300;
-    color = `#fff000`;
-    offsetX = cvs1.width / 2 - this.size / 2;
-    offsetY = cvs1.height / 2 - this.size /2 ;
+    // color = `#fff000`;
+    color = 'rgba(255, 200, 0, 0.8)';
+    offsetX = cvs1.width / 2;
+    offsetY = cvs1.height / 2 ;
+    scale = 0;
 
     constructor(index, offsetX = this.offsetX, offsetY = this.offsetY, size=this.size){
         this.index = index;
         this.offsetX = offsetX ?? this.offsetX;
         this.offsetY = offsetY ?? this.offsetY;
         this.size = size ?? this.size;
-        this.draw();
     }
 
     draw(){
+        ctx.resetTransform();
+
+        ctx.translate(this.offsetX, this.offsetY);
+        ctx.scale(this.scale, this.scale);
+        ctx.translate(-this.offsetX, -this.offsetY);
+
         ctx.fillStyle = this.color;
-        ctx.fillRect(this.offsetX, this.offsetY, this.size, this.size);
+        ctx.fillRect(this.offsetX - this.size / 2, this.offsetY - this.size /2, this.size, this.size);
         ctx.save();
+
         ctx.fillStyle = 'black';
         ctx.font = 'normal 30px sans-serif';
-        ctx.fillText(this.index, this.offsetX+30, this.offsetY+60);
+        ctx.fillText(this.index, this.offsetX - this.size / 2 + 30, this.offsetY - this.size / 2 + 60);
         ctx.restore();
+
+        ctx.resetTransform();
     }
 }
