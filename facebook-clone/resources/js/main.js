5042ad025dc3b184fa0da10af4b1dce811637bdb
sunday00 <kind65505256@gmail.com>
2021-06-07 04:48:28 +0900

    header and notice

--- facebook-clone/resources/js/main.js
+++ facebook-clone/resources/js/main.js
@@ -1,1 +1,10 @@
-console.log('das')
+// DEV:
+const noticeUl = document.querySelector('.notice ul');
+const noticeLi = document.querySelector('.notice li');
+
+for(let i=0; i<30; i++){
+    let li = noticeLi.cloneNode(true);
+    noticeUl.append(li);
+}
+
+
