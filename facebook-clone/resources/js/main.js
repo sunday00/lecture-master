92285c4d1238576d0edfbfd2f0f5901675e7fce4
sunday00 <kind65505256@gmail.com>
2021-06-08 00:35:07 +0900

    content and comment

--- facebook-clone/resources/js/main.js
+++ facebook-clone/resources/js/main.js
@@ -7,4 +7,13 @@ for(let i=0; i<30; i++){
     noticeUl.append(li);
 }
 
+const commentContainer = document.querySelector('#contents-container .comment-section');
+const comment = document.querySelector('#contents-container .comment-section .id');
+const commentLengths = [
+    Math.ceil(Math.random() * 3),
+]
 
+for(let i=0; i<commentLengths; i++){
+    let cm = comment.cloneNode(true);
+    commentContainer.append(cm);
+}
