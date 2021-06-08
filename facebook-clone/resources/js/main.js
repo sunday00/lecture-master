d71aafc03d11259eed63740d5980e136a68059c9
sunday00 <kind65505256@gmail.com>
2021-06-08 21:36:50 +0900

    js basic

--- facebook-clone/resources/js/main.js
+++ facebook-clone/resources/js/main.js
@@ -2,7 +2,7 @@
 const noticeUl = document.querySelector('.notice ul');
 const noticeLi = document.querySelector('.notice li');
 
-for(let i=0; i<30; i++){
+for(let i=0; i<10; i++){
     let li = noticeLi.cloneNode(true);
     noticeUl.append(li);
 }
@@ -17,3 +17,75 @@ for(let i=0; i<commentLengths; i++){
     let cm = comment.cloneNode(true);
     commentContainer.append(cm);
 }
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    
+    //INFO: init.
+    resizeHandler();
+
+    // FUNC: notification
+    const bell = document.querySelector('.bell');
+
+    const notification = function(){
+        this.classList.toggle('active');
+    }
+
+    bell.addEventListener('click', notification);
+    
+    // FUNC: feed.addEvent delegation
+    function delegation (e) {
+        let t = e.target;
+        console.log(t);
+
+        while(!t.dataset.name){
+            t = t.parentNode;
+
+            if( t.nodeName === 'BODY' ){
+                t = null;
+                return;
+            }
+        }
+
+        //METHOD: like button
+        if(t.dataset.name === 'like'){
+            console.log("like~~");
+
+        // METHOD: more button    
+        } else if (t.dataset.name === 'more') {
+            t.querySelector('.hidden-menu').classList.toggle('active');
+        }
+    }
+
+    const feeds = document.querySelector('#contents-container');
+
+    feeds.addEventListener('click', delegation);
+});
+
+//FUNC: scroll event
+function scrollFunc (){
+    let bodyH = document.body.scrollHeight;
+    let currentY = window.pageYOffset;
+
+    if (bodyH <= currentY + window.innerHeight){
+        // TODO: load ajax
+    }
+    
+}
+
+window.addEventListener('scroll', scrollFunc);
+
+//FUNC: resize window
+function resizeHandler (){
+    const leftBox = document.querySelector('.left-box');
+    const middleBox = document.querySelector('.middle-box');
+    const rightBox = document.querySelector('.right-box');
+
+    const follow = document.querySelector('#header .inner');
+
+    leftBox.style.left = follow.style.left;
+    middleBox.style.left = leftBox.offsetWidth + 20 + 'px';
+    rightBox.style.left = leftBox.offsetLeft + leftBox.offsetWidth + 20 + middleBox.offsetWidth + 20 + 'px';
+}
+
+window.addEventListener('resize', resizeHandler);
