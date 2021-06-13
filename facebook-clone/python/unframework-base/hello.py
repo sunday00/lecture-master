f09ea92c7e5ad8880fe3c377be4130c654d8fab9
sunday00 <kind65505256@gmail.com>
2021-06-13 21:35:38 +0900

    type

---
+++ facebook-clone/python/unframework-base/hello.py
@@ -0,0 +1,15 @@
+'''
+hello world
+'''
+
+true = True
+false = False
+
+a = 10
+b = 10 + 2j
+
+print( dir(a) )
+print( b )
+print( type(b) ) #complect type. 복소수. https://www.youtube.com/watch?v=tBl2KLPlZjU
+
+
