7d0df9948fe328784db2beb14fec376b7d19fd36
sunday00 <kind65505256@gmail.com>
2021-06-22 22:20:41 +0900

    import

---
+++ activate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+PWD=`pwd`
+
+echo $PWD
+activate () {
+    . $PWD/facebook-clone/python/django-crash/bin/activate
+}
+
+activate
+
+echo "
+
+
+
+
+
+
+
+
+
+===============
+if not activated,
+
+source facebook-clone/python/django-crash/bin/activate
+===========
+
+disable : deactivate
+
+"
---
+++ facebook-clone/python/unframework-base/moduleAndPackage.py
@@ -0,0 +1,8 @@
+import random
+
+ri = random.randint(0, 10)
+print(ri)
+
+rl = [12,423,345,6,567,2,2314,1234]
+print(random.choice(rl))
+
