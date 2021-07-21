bb3f69ce0c47cf2e9f87e9b245c8e889ac330d83
sunday00 <kind65505256@gmail.com>
2021-07-21 18:52:40 +0900

    wsl comming to valet

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require __DIR__.'/vendor/autoload.php';
 include('includes/functions.php');
 
+define('__DOCUMENT_ROOT__', $_SERVER['DOCUMENT_ROOT']);
+
 ?>
 
 <!DOCTYPE html>
@@ -11,11 +13,11 @@ include('includes/functions.php');
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP with swagger crash</title>
-  <link rel="stylesheet" href="https://unpkg.com/wingcss"/>
+  <?php include(__DOCUMENT_ROOT__.'/themes/styles.php'); ?>
 </head>
 <body>
   <main class="container">
-    <h1>CRUD</h1>
+    <h1><em class="fas fa-check-circle"></em> CRUD</h1>
 
     <?php
       formatcode(selectAll());
