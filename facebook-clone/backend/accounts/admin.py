d589ebf52a80fdba92986ef3bd3b446d0eef82aa
sunday00 <kind65505256@gmail.com>
2021-07-24 21:07:54 +0900

    profile

--- facebook-clone/backend/accounts/admin.py
+++ facebook-clone/backend/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Proflie
 
 # Register your models here.
+@admin.register(Proflie)
+class ProfileAdmin(admin.ModelAdmin):
+  list_display = ['id', 'nick', 'user']
+  list_display_links = ['id', 'nick', 'user']
+  search_fields = ['nick']
--- facebook-clone/backend/accounts/models.py
+++ facebook-clone/backend/accounts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.deletion import CASCADE
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+import re
 
 # Create your models here.
+def user_path(instance, filename: str):
+  from random import choice
+  import string
+  arr = [choice(string.ascii_letters) for _ in range(8)]
+  pid = ''.join(arr)
+  ext = filename.split('.')[-1]
+  return 'accounts/{}/{}.{}'.format(instance.user.username, pid, ext)
+
+class Proflie(models.Model):
+  GENDER_C = (
+    ('not picked', 'not picked'),
+    ('female', 'female'),
+    ('male', 'male'),
+  )
+
+  user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=CASCADE)
+  nick = models.CharField('nick', max_length=30, unique=True)
+  picture = ProcessedImageField(
+      upload_to=user_path,
+      processors=[ResizeToFill(150, 150)],
+      format='JPEG',
+      options={'quality' : 90}, blank=True,
+    )
+  about = models.CharField(max_length=300, blank=True)
+  gender = models.CharField('gender', max_length=10, choices=GENDER_C, default='N')
+
