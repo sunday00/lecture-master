8dec1f27aa691fdcdb06aa00229c25719a403518
sunday00 <kind65505256@gmail.com>
2021-07-30 00:05:49 +0900

    model post and migrate

--- facebook-clone/backend/config/urls.py
+++ facebook-clone/backend/config/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda r: redirect('post:post_list'), name='root'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 
