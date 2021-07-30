f9d5df49af318ea120df48d8f88d272c6be9d5e0
sunday00 <kind65505256@gmail.com>
2021-07-30 11:52:00 +0900

    dom render order

--- l4-cordova/statics/app.js
+++ l4-cordova/statics/app.js
@@ -4,13 +4,30 @@
 
 function getPages(){
   let app = $.sammy('#app', function(){
-    this.use('template');
+    // this.use('template');
 
-    this.get('#/', function(ctx){
+    let home = this.get('#/', function(ctx, next){
       let str = location.href.toLowerCase();
       ctx.app.swap('');
+
       ctx.render('/pages/home.template', {})
-        .appendTo(ctx.$element());
+        .appendTo(ctx.$element())
+        .then(function(c, n){
+          $('#test').html('hello testing?');
+          console.log('3', 'this is called very lazy, even after main layout html rendered');
+        });
+
+        ctx.trigger('launch', {action: 1});
+
+        next();
+    }, function(ctx, next){
+      console.log('2')
+
+      next();
+    });
+
+    this.bind('launch', function(e, d){
+      console.log(d.action);
     });
 
     this.get('#/about', function(ctx){
