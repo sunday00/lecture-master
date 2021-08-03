b3a8df4e6275ac79e684689a81a1dbf056493f05
sunday00 <kind65505256@gmail.com>
2021-08-03 09:58:21 +0900

    small fix

--- l4-cordova/statics/app.js
+++ l4-cordova/statics/app.js
@@ -1,5 +1,6 @@
 (function($){
-  getPages();
+  // getPages();
+  getAllPages();
 })(jQuery);
 
 function getPages(){
@@ -10,11 +11,18 @@ function getPages(){
       let str = location.href.toLowerCase();
       ctx.app.swap('');
 
-      ctx.render('/pages/home.template', {})
+      ctx.render('/templates/pages/home.html', {})
         .appendTo(ctx.$element())
         .then(function(c, n){
-          $('#test').html('hello testing?');
           console.log('3', 'this is called very lazy, even after main layout html rendered');
+
+          $.get('http://dev-drw-l3-oop.web/api/v1/pages/read/home')
+            .then(res => {
+              $('header nav .navbar-nav .nav-item a').removeClass('active');
+              $('header nav .navbar-nav .nav-item.home a').addClass('active');
+
+              $('article#content').html(res.data.content);
+            });
         });
 
         ctx.trigger('launch', {action: 1});
@@ -30,13 +38,55 @@ function getPages(){
       console.log(d.action);
     });
 
-    this.get('#/about', function(ctx){
+    this.get('#/:slug', function(ctx){
       let str = location.href.toLowerCase();
       ctx.app.swap('');
-      ctx.render('/pages/about.template', {})
-        .appendTo(ctx.$element());
+      ctx.render(`/templates/pages/${ctx.params.slug}.html`, {})
+        .appendTo(ctx.$element())
+        .then(function(){
+
+          $.get(`http://dev-drw-l3-oop.web/api/v1/pages/read/${ctx.params.slug}`)
+            .then(res => {
+              // $('header nav .navbar-nav .nav-item a').removeClass('active');
+              // $(`header nav .navbar-nav .nav-item.${ctx.params.slug} a`).addClass('active');
+
+              $('article#content').html(res.data.content);
+            });
+
+        });
+    });
+
+    this.before('.*', function(){
+      let hash = location.hash; // url http://abc.com#/aaa -> hash: #/aaa
+      
+      $('header nav .navbar-nav .nav-item a').removeClass('active');
+      $(`header nav .navbar-nav .nav-item a[href="${hash}"]`).addClass('active');
     });
   });
 
   app.run('#/');
+}
+
+function getAllPages(){
+  $.get('http://dev-drw-l3-oop.web/api/v1/pages')
+    .then(res => {
+      res.data.forEach(d => {
+        let slug = d.slug === null ? '' : d.slug;
+        let active = location.hash === '#/'+slug ? 'active' : '';
+        let ariaCurrent = active === 'active' ? 'page' : 'false';
+  
+        $('header nav .navbar-nav').append(`
+          <li class="nav-item ${d.title}">
+            <a class="nav-link ${active}" aria-current="${ariaCurrent}" href="#/${slug}">
+              ${d.title.replace(/./, (c) => c.toUpperCase())}
+            </a>
+          </li>
+        `);
+      });
+
+      getPages();
+    }).catch((xhr, textStatus, err) => {
+      // let data = JSON.parse(err.responseText);
+      console.log(xhr, textStatus, err)
+    });
 }
