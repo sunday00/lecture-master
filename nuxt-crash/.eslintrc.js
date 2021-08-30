1f4be40476052e146af502fa79db125ddc6384d8
sunday00 <kind65505256@gmail.com>
2021-08-30 10:38:17 +0900

    nuxt link

--- nuxt-crash/.eslintrc.js
+++ nuxt-crash/.eslintrc.js
@@ -2,18 +2,26 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'never',
+      exports: 'never',
+      functions: 'never',
+    }],
+  },
 }
--- nuxt-crash/components/TheNavbar.vue
+++ nuxt-crash/components/TheNavbar.vue
@@ -1,6 +1,9 @@
 <template>
   <nav class="flex justify-between bb b--white-10">
-    <a class="link white-70 hover-white no-underline flex items-center pa3" href="/">
+    <nuxt-link
+      class="link white-70 hover-white no-underline flex items-center pa3"
+      :to="{name: 'index'}"
+    >
       <svg
         class="dib h1 w1"
         data-icon="grid"
@@ -10,9 +13,9 @@
         <title>Super Normal Icon Mark</title>
         <path d="M2 2 L10 2 L10 10 L2 10z M12 2 L20 2 L20 10 L12 10z M22 2 L30 2 L30 10 L22 10z M2 12 L10 12 L10 20 L2 20z M12 12 L20 12 L20 20 L12 20z M22 12 L30 12 L30 20 L22 20z M2 22 L10 22 L10 30 L2 30z M12 22 L20 22 L20 30 L12 30z M22 22 L30 22 L30 30 L22 30z" />
       </svg>
-    </a>
+    </nuxt-link>
     <div class="flex-grow pa3 flex items-center">
-      <a class="f6 link dib white dim mr3 mr4-ns" href="post">
+      <a class="f6 link dib white dim mr3 mr4-ns" href="/posts/balut">
         Post
       </a>
       <a class="f6 link dib white dim mr3 mr4-ns" href="#0">Sign In</a>
