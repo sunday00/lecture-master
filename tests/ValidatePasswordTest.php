9a5fd9f247211c9a7905b9501657336d8086b856
sunday00 <kind65505256@gmail.com>
2021-08-31 11:30:50 +0900

    validate pawd

---
+++ tests/ValidatePasswordTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ValidatePasswordTest extends TestCase
+{
+    public function testValidLength()
+    {
+        $valPass = new ValidatePassword();
+        $this->assertFalse($valPass->validLength('1234'));
+    }
+}
