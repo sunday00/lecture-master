d0bbbc3b47bba953677f3b49ddb647bbd236d7e1
sunday00 <kind65505256@gmail.com>
2021-09-03 18:14:44 +0900

    mod and del

--- lib/mutations/UserMutation.php
+++ lib/mutations/UserMutation.php
@@ -4,18 +4,14 @@ namespace Lib\mutations;
 
 use App\Models\User;
 use GraphQL\Type\Definition\Type;
-use GraphQL\Type\Definition\ObjectType;
-use Lib\types\UserType;
 
 class UserMutation
 {
   public function getMutation($types, $param = null)
   {
     return $this->getAdd($types['user'])
-        // + $this->getAdd()
-        // + $this->getAdd()
-    ;   // + $this->getAdd()
-        
+        + $this->getEdit($types['user'])
+        + $this->getDelete($types['user'])
     ;
   }
 
@@ -41,4 +37,48 @@ class UserMutation
       ]
     ];
   }
+
+  private function getEdit($type)
+  {
+    return [
+      'editUser' => [
+        'type' => $type,
+        'args' => [
+          'id'          => Type::nonNull(Type::int()),
+          'first_name'  => Type::string(),
+          'last_name'   => Type::string(),
+          'email'       => Type::string(),
+        ],
+        'resolve' => function($root, $args){
+          $user = User::find($args['id']);
+          $user->update([
+            'first_name'  => $args[  'first_name' ],
+            'last_name'   => $args[  'last_name'  ],
+            'email'       => $args[  'email'      ],
+          ]);
+          $user->save();
+          return $user->toArray();
+        }
+      ]
+    ];
+  }
+
+  private function getDelete($type)
+  {
+    return [
+      'deleteUser' => [
+        'type' => $type,
+        'args' => [
+          'id' => Type::nonNull(Type::int()),
+        ],
+        'resolve' => function($root, $args){
+          $user = User::find($args['id']);
+          $user->delete();
+          return $user->toArray();
+        }
+      ]
+    ];
+  }
+
 }
+
