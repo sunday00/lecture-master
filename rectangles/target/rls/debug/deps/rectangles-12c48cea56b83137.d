75b68a2b3db12b5d03ab4d73b747d32652bd1807
sunday00 <kind65505256@gmail.com>
2021-09-13 15:52:16 +0900

    re

--- rectangles/target/rls/debug/deps/rectangles-12c48cea56b83137.d
+++
@@ -1,5 +0,0 @@
-d:\sunday00\private\2020\rust-tutorial\rectangles\target\rls\debug\deps\rectangles-12c48cea56b83137.rmeta: src\main.rs
-
-d:\sunday00\private\2020\rust-tutorial\rectangles\target\rls\debug\deps\rectangles-12c48cea56b83137.d: src\main.rs
-
-src\main.rs:
--- rectangles/target/rls/debug/deps/rectangles-b6e80dfb3a9290f5.d
+++
@@ -1,5 +0,0 @@
-d:\sunday00\private\2020\rust-tutorial\rectangles\target\rls\debug\deps\rectangles-b6e80dfb3a9290f5.rmeta: src\main.rs
-
-d:\sunday00\private\2020\rust-tutorial\rectangles\target\rls\debug\deps\rectangles-b6e80dfb3a9290f5.d: src\main.rs
-
-src\main.rs:
