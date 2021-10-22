a08e4e30482bbd394362e118db334861f0af7dcd
sunday00 <kind65505256@gmail.com>
2021-10-22 10:45:58 +0900

    run fn

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -2,24 +2,20 @@ use std::env;
 use std::process;
 use std::fs::File;
 use std::io::prelude::*;
+use std::error::Error;
 
 fn main () {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Err <parsing config>: {}", err);
+        println!("\n\nErr <parsing config>: {}\n\n", err);
         process::exit(1);
     });
 
-    println!("Searching for \x1b[38;5;14m{}\x1b[0m", config.query);
-    println!("In file \x1b[38;5;14m{}\x1b[0m", config.filename);
+    println!("\nSearching for \x1b[38;5;14m{}\x1b[0m", config.query);
+    println!("In file \x1b[38;5;14m{}\x1b[0m\n", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("Can't read file.");
-
-    println!("With text:\n{}", contents);
+    run(config);
 }
 
 struct Config {
@@ -31,10 +27,10 @@ impl Config {
     fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err(
-                "\n\n
+                "
                 The number of arguments is not enough. Expecting string you're looking for and filename.
                 ex>>> cargo run ABCD filename.txt ;
-                \n\n"
+                "
             )
         }
 
@@ -45,3 +41,15 @@ impl Config {
     }    
 }
 
+fn run (config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)? ;
+        // .expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)? ;
+        // .expect("Can't read file.");
+
+    println!("With text:\n{}\n", contents);
+
+    Ok(())
+}
