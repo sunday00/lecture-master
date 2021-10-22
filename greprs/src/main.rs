d1f8e34db3a3d6fd144b9f3de9cd209c044942d8
sunday00 <kind65505256@gmail.com>
2021-10-22 09:40:36 +0900

    grepers remove primitive obsession

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -5,14 +5,12 @@ use std::io::prelude::*;
 fn main () {
     let args: Vec<String> = env::args().collect();
     
-    // let query = &args[1];
-    // let filename = &args[2];
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for \x1b[38;5;14m{}\x1b[0m", query);
-    println!("In file \x1b[38;5;14m{}\x1b[0m", filename);
+    println!("Searching for \x1b[38;5;14m{}\x1b[0m", config.query);
+    println!("In file \x1b[38;5;14m{}\x1b[0m", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("Can't read file.");
@@ -20,6 +18,14 @@ fn main () {
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    ( &args[1], &args[2] )
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
