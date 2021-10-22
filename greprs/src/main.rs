cc56287faef78f2f946b6fe5c94a7b01cc88cc69
sunday00 <kind65505256@gmail.com>
2021-10-22 10:12:33 +0900

    process down

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
+use std::process;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main () {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Err <parsing config>: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for \x1b[38;5;14m{}\x1b[0m", config.query);
     println!("In file \x1b[38;5;14m{}\x1b[0m", config.filename);
@@ -24,11 +28,20 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err(
+                "\n\n
+                The number of arguments is not enough. Expecting string you're looking for and filename.
+                ex>>> cargo run ABCD filename.txt ;
+                \n\n"
+            )
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
     
-        Config { query, filename }
+        Ok(Config { query, filename })
     }    
 }
 
