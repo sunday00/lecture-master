3daefefd457ad8ad116aec1a18e5a340b7ed601b
sunday00 <kind65505256@gmail.com>
2021-10-22 17:53:42 +0900

    insensitive case

--- greprs/src/lib.rs
+++ greprs/src/lib.rs
@@ -1,10 +1,12 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use regex::*;
 
 pub struct Config {
   pub query: String,
   pub filename: String,
+  pub case_sensitive: bool,
 }
 
 impl Config {
@@ -20,8 +22,23 @@ impl Config {
 
       let query = args[1].clone();
       let filename = args[2].clone();
+
+      // let case_sensitive = std::env::var("CASE_SENSITIVE").is_err();
+      let mut case_sensitive: bool = true;
+
+      if args.len() > 3 {
+        for i in 3..args.len() {
+          let splited: Vec<&str> = args[i].split("=").collect();
+          match splited[0] {
+            "--ignore-case" => {
+              case_sensitive = splited[1] != "1";
+            },
+            _ => {}
+          }
+        }
+      }
   
-      Ok(Config { query, filename })
+      Ok(Config { query, filename, case_sensitive })
   }    
 }
 
@@ -33,12 +50,19 @@ pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
   f.read_to_string(&mut contents)? ;
       // .expect("Can't read file.");
 
-  let results = search(&config.query, &contents);
+  let results = if config.case_sensitive {
+    search(&config.query, &contents)
+  } else {
+    search_case_insensitive(&config.query, &contents)
+  };
+
   if results.len() > 0 {
-    for line in results {
+    for line in &results {
       println!("{}", line);
     }
     // println!("With text:\n{}\n", contents);
+
+    println!("\n=== total : {} times found ===", results.len());
   } else {
     println!(
       "\x1b[38;5;208mThe string \x1b[38;5;14m{} \x1b[38;5;208mis not here in file \x1b[38;5;14m{}\x1b[38;5;208m.\x1b[0m",
@@ -53,13 +77,36 @@ pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<String> {
   let mut results = Vec::new();
 
-  for line in contents.lines(){
+  for (i, line) in contents.lines().enumerate() {
     if line.contains(query){
       let colored_query = format!(
         "\x1b[38;5;10m{}\x1b[0m", query
       );
     
-      let colored_line = line.replace(query, &colored_query);
+      let colored_line = format!( "{} >>> {}", i + 1, line.replace(query, &colored_query) );
+
+      results.push(colored_line);
+    }
+  }
+
+  results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<String> {
+  let lo_query = query.to_lowercase();
+  let mut results = Vec::new();
+
+  for (i, line) in contents.lines().enumerate() {
+    if line.to_lowercase().contains(&lo_query){
+      
+      let pattern = format!("(?i){}", &lo_query);
+      let re = Regex::new(&pattern).unwrap();
+      
+      let colored_query = format!(
+        "\x1b[38;5;10m{}\x1b[0m", &query
+      );
+      
+      let colored_line = format!( "{} >>> {}", i + 1, re.replace_all(&line, &colored_query) );
 
       results.push(colored_line);
     }
@@ -73,16 +120,32 @@ mod test {
   use super::*;
 
   #[test]
-  fn one_result(){
+  fn case_sensitive(){
     let query = "duct";
     let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
     assert_eq!(
-      vec!["safe, fast, productive."], 
+      vec!["2 >>> safe, fast, pro\u{1b}[38;5;10mduct\u{1b}[0mive."], 
       search(query, contents)
+    )
+  }
+
+  #[test]
+  fn case_insensitive(){
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(
+      vec!["1 >>> \u{1b}[38;5;10mrUsT\u{1b}[0m:", "4 >>> T\u{1b}[38;5;10mrUsT\u{1b}[0m me."], 
+      search_case_insensitive(query, contents)
     )
   }
 }
