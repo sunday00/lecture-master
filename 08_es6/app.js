8489e97d678c054f5c2e38e39c2424259a2c51d4
sunday00 <kind65505256@gmail.com>
2021-11-02 18:12:13 +0900

    computed style

--- 08_es6/app.js
+++ 08_es6/app.js
@@ -1,3 +1,4 @@
+{
 // const allImages = document.getElementsByTagName('img');
 
 // for (let img of allImages) {
@@ -16,3 +17,27 @@
 // for (let link of links) {
 //     console.log(link.href)
 // }
+}
+
+const banner = document.getElementById("banner")
+
+let imgs = document.getElementsByTagName('img');
+
+let firstLink = document.querySelector('a')
+console.log( firstLink.href ,":" ,  firstLink.getAttribute('href') )
+
+let h1 = document.querySelector('h1')
+h1.style.color = 'tomato'
+h1.style.fontSize = '3em'
+
+let links = document.querySelectorAll('a');
+for (let a of links){
+  a.style.color = "#4455BC"
+  a.style.textDecorationColor = "#BB5544"
+  a.style.textDecorationStyle = "wavy"
+}
+
+console.log( window.getComputedStyle(document.body).fontSize )
+// style로 직접 지정한 게 없을때는 browser 기본값을 return.
+// dynamic 하게 옆에 글자높이랑 맞추고 싶은데, 옆에 dom은 style로 지정한게 없어 null 이 
+// 리턴되거나 할 때 이용하면 좋을 듯.
