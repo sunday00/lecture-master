83c2d8b741b9aa06933d8c6f7fa0a70775d46867
sunday00 <kind65505256@gmail.com>
2021-11-10 17:56:19 +0900

    thread

---
+++ concurrent/src/modules/m1.rs
@@ -0,0 +1,32 @@
+use std::thread;
+use std::time::Duration;
+
+pub fn run () {
+  thread::spawn(|| {
+    for i in 1..10 {
+      println!("hi number {} from the spawn thread!", i);
+      thread::sleep(Duration::from_millis(1));
+    }
+  });
+
+  for i in 1..5 {
+    println!("hi number {} from the main thread!", i);
+    thread::sleep(Duration::from_millis(1));
+  }
+}
+
+pub fn run2 () {
+  let handle = thread::spawn(|| {
+    for i in 1..10 {
+      println!("hi number {} from the spawn thread!", i);
+      thread::sleep(Duration::from_millis(1));
+    }
+  });
+
+  for i in 1..5 {
+    println!("hi number {} from the main thread!", i);
+    thread::sleep(Duration::from_millis(1));
+  }
+
+  handle.join().unwrap();
+}
