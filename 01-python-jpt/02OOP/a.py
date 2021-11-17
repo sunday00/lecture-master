d7e99d6bdb38c800242c48645c97e7a9900f39ee
sunday00 <kind65505256@gmail.com>
2021-11-17 21:28:30 +0900

    abs

--- 01-python-jpt/02OOP/a.py
+++ 01-python-jpt/02OOP/a.py
@@ -4,8 +4,9 @@ from f import *
 n = a2.add_numbers(a2.variable, 1)
 print(n)
 
+n2 = a2.subtract_numbers(a2.variable, 1)
+
 print(a3.multiple_numbers(2, 3))
 
 print( a4.variable, a4.__name__ )
-
 
--- 01-python-jpt/02OOP/a2.py
+++ 01-python-jpt/02OOP/a2.py
@@ -2,3 +2,6 @@ variable: int = 3
 
 def add_numbers(x:int , y:int) -> int :
     return x + y
+
+def subtract_numbers(x:int, y:int) -> int:
+    return x - y
