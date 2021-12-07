a1c9a717000d064aac69db4bbc4ac06b4a5b455a
sunday00 <kind65505256@gmail.com>
2021-12-07 17:04:37 +0900

    ejs integration

--- 16_fw/tpl-scratch.js
+++ 16_fw/tpl-scratch.js
@@ -4,11 +4,34 @@ const path = require('path')
 const app = express()
 const port = 8088
 
+const datas = require('./data.json')
+
 app.set('view engine', 'ejs')
   .set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.get('/rand', (req, res) => {
+  res.render('random', {
+    num: Math.floor(Math.random() * 10) + 1
+  })
+})
+
+app.get('/r/:subreddit', (req, res) => {
+  const { subreddit } = req.params
+  const data = datas[subreddit]
+  
+  data ? res.render('subreddit', {
+    ...data
+  }) : res.send('404')
+})
+
+app.get('/cats', (req, res) => {
+  res.render('cats', {
+    cats: ['Blue','Monty','Rocket','Winston','Nya-on','ChamCi',]
+  })
 })
 
 app.listen(port, () => {
