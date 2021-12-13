4c9ed3d23068c2d5aace2a43755cfa83a8860bb9
sunday00 <kind65505256@gmail.com>
2021-12-13 13:30:18 +0900

    vaector

--- ytb/common-known/src/collections/mod.rs
+++ ytb/common-known/src/collections/mod.rs
@@ -1,9 +1,22 @@
 pub mod arr;
+pub mod vector;
 
 pub fn run(module: String, arg: String) {
   if module == "length" {
     arr::length(arg);
   } else if module == "slice" {
     arr::slice(arg);
-  } ;
+  } else if module == "vec" {
+    vector::deal(arg);
+  } else if module == "vec_slice" {
+    vector::slice(arg);
+  } else if module == "capacity" {
+    vector::capacity(arg);
+  } else if module == "capacity-small" {
+    vector::capacity_small(arg);
+  } else if module == "capacity-large" {
+    vector::capacity_large(arg);
+  } else if module == "capacity-type" {
+    vector::capacity_type(arg);
+  };
 }
