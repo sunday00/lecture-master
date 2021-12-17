d16cc7ae7af9e0d9bf5006bb2fc9371052f2e99f
sunday00 <kind65505256@gmail.com>
2021-12-17 16:12:47 +0900

    mongo db

---
+++ 16_fw/mongodata.js
@@ -0,0 +1,13 @@
+[
+  {
+    name: "Blur Steele",
+    breed: "Scottish fold",
+    age: 6,
+    dogFriendly: false
+  },{
+    name: "Chamci",
+    breed: "Korean street cat",
+    age: 10,
+    catFriendly: true
+  },
+]
