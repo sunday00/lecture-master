c8d027ed987513c226e17308fc32a8fca3bae703
sunday00 <kind65505256@gmail.com>
2021-12-17 14:19:21 +0900

    rest and swagger

--- 16_fw/tpl-scratch.js
+++ 16_fw/tpl-scratch.js
@@ -38,7 +38,8 @@ const options = {
 const specs = swaggerJsdoc(options);
 
 // app.use(express.static('public'))
-app.use(express.static(path.join(__dirname, 'public')))
+app
+  .use(express.static(path.join(__dirname, 'public')))
   .use(express.urlencoded({ extended: true }))
   .use (express.json())
   .use (methodOverride((req, res) => {
@@ -75,30 +76,89 @@ let comments = [
   },
 ]
 
+/**
+ * @swagger
+ *  /comments:
+ *    get:
+ *      tags:
+ *        - comments
+ *      description: comments
+ *      produces:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: 코멘트 리스트
+ */
 app.get('/comments', (req, res) => {
-  res.render('comments/index', {
-    comments
-  })
+  // res.render('comments/index', {
+  //   comments
+  // })
+  res.send(comments)
 })
 
 app.get('/comments/new', (req, res) => {
   res.render('comments/form')
 })
 
-app.post('/comments', (req, res) => {
+/**
+ * @swagger
+ *  /comments:
+ *    post:
+ *      tags:
+ *        - comments
+ *      description: comments
+ *      requestBody:
+ *        required: true
+ *        description: 코멘트 내용 - 작성자, 코멘트 내용
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                username:
+ *                  type: string
+ *                comment:
+ *                  type: string
+ *      responses:
+ *        200:
+ *          description: 새 코멘트가 추가된 리스트
+ */
+app.post('/comments', async (req, res) => {
+  await console.log(req.body)
   const { username, comment } = req.body
   // comments.push({ id: comments[comments.length - 1].id + 1, username, comment })
-  comments.push({ id: uuid(), username, comment })
-  res.redirect('/comments')
+  await comments.push({ id: uuid(), username, comment })
+  // res.redirect('/comments')
+  await res.send(comments)
 })
 
+/**
+ * @swagger
+ *  /comments/{id}:
+ *    get:
+ *      tags:
+ *        - comments
+ *      description: comments one
+ *      produces:
+ *        - application/json
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: 코멘트 한개
+ */
 app.get('/comments/:id', (req, res) => {
   const {id} = req.params
   // const comment = comments.find(c => c.id === parseInt(id))
   const comment = comments.find(c => c.id === id)
-  res.render('comments/index', {
-    comments: comment ? [ comment ] : []
-  })
+  // res.render('comments/index', {
+  //   comments: comment ? [ comment ] : []
+  // })
+  res.send(comment)
 })
 
 
@@ -111,20 +171,72 @@ app.get('/comments/:id/edit', (req, res) => {
   })
 })
 
+/**
+ * @swagger
+ *  /comments/{id}:
+ *    patch:
+ *      tags:
+ *        - comments
+ *      description: comments edit
+ *      produces:
+ *        - application/json
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *            type: string
+ *      requestBody:
+ *        required: true
+ *        description: 코멘트 내용 - 작성자, 코멘트 내용
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                username:
+ *                  type: string
+ *                comment:
+ *                  type: string
+ *      responses:
+ *        200:
+ *          description: 코멘트 수정된 후 리스트
+ */
 app.patch('/comments/:id', (req, res) => {
   const { id } = req.params
   const comment = comments.find(c => c.id === id)
   comment.comment = req.body.comment
   comment.username = req.body.username
 
-  res.redirect(`/comments/${id}`)
+  // res.redirect(`/comments/${id}`)
+  res.send(comments)
 })
 
+/**
+ * @swagger
+ *  /comments/{id}:
+ *    delete:
+ *      tags:
+ *        - comments
+ *      description: comments remove
+ *      produces:
+ *        - application/json
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *            type: string
+ *      responses:
+ *        200:
+ *          description: 코멘트 삭제
+ */
 app.delete('/comments/:id', (req, res) => {
   const { id } = req.params
   comments = comments.filter(c => c.id !== id)
 
-  res.redirect(`/comments/`)
+  // res.redirect(`/comments/`)
+  res.send(comments)
 })
 
 app.get('/', (req, res) => {
