16c738e1d22824b326757726d79730d76b587ace
sunday00 <kind65505256@gmail.com>
2021-12-21 11:02:07 +0900

    hash and btree map

--- ytb/common-known/src/collections/mod.rs
+++ ytb/common-known/src/collections/mod.rs
@@ -1,25 +1,25 @@
 pub mod arr;
 pub mod vector;
 pub mod tuple;
+pub mod hm;
 
 pub fn run(module: String, arg: String) {
-  if module == "length" {
-    arr::length(arg);
-  } else if module == "slice" {
-    arr::slice(arg);
-  } else if module == "vec" {
-    vector::deal(arg);
-  } else if module == "vec_slice" {
-    vector::slice(arg);
-  } else if module == "capacity" {
-    vector::capacity(arg);
-  } else if module == "capacity-small" {
-    vector::capacity_small(arg);
-  } else if module == "capacity-large" {
-    vector::capacity_large(arg);
-  } else if module == "capacity-type" {
-    vector::capacity_type(arg);
-  } else if module == "spread" {
-    tuple::spread(arg);
-  };
+  let module: &str = &module.to_string();
+  match module {
+    "length" => arr::length(arg),
+    "slice" => arr::slice(arg),
+    "vec" => vector::deal(arg),
+    "vec-slice" => vector::slice(arg),
+    "capacity" => vector::capacity(arg),
+    "capacity-small" => vector::capacity_small(arg),
+    "capacity-large" =>  vector::capacity_large(arg),
+    "capacity-type" => vector::capacity_type(arg),
+    "spread" => tuple::spread(arg),
+    "noBtree" => hm::hm(arg),
+    "btree" => hm::btree(arg),
+    "k" => hm::k(arg),
+    "lotto" => hm::lotto(arg),
+    "library" => hm::library(arg),
+    _ => arr::length(arg),
+  }
 }
