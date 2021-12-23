527974ac8b5bc3e94c5bc9ddfe3b6161f8ed7dba
sunday00 <kind65505256@gmail.com>
2021-12-23 12:45:31 +0900

    iterate and mutate

--- ytb/common-known/src/iterable/iterator.rs
+++ ytb/common-known/src/iterable/iterator.rs
@@ -1,3 +1,16 @@
 pub fn looping(_: String) {
-  
+  let mut v0 = vec![1, 2, 3];
+  let va: Vec<u8> = v0.iter().map(|x| x + 1).collect();
+  let vb: Vec<u8> = v0.clone().into_iter().map(|x| x + 1).collect();
+
+  v0.iter_mut().for_each(|x| *x += 1);
+
+  // iter: just ref and return new iterable
+  // into_iter: mutate original then original gone (aka: moved)
+    // so, when I need to keep original, clone original
+  // into_mut: mutate original itself. keep it!
+
+  println!("{:?}", va);
+  println!("{:?}", vb);
+  println!("{:?}", v0);
 }
