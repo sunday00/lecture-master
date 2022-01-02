c9ce4dd9a7d9e525e8a4410094c9909c9126bff0
sunday00 <kind65505256@gmail.com>
2022-01-02 19:26:20 +0900

    model

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './boards.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
@@ -6,7 +7,15 @@ export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
   @Get()
-  getAll() {
+  getAll(): Board[] {
     return this.boardService.getAll();
+  }
+
+  @Post('/create')
+  create(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Board {
+    return this.boardService.create(title, description);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Board, BoardsStatus } from './boards.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
   //TODO: this is dummy. should replace with model/repository/entities
-  private boards = [];
+  private boards: Board[] = [];
 
-  getAll() {
+  getAll(): Board[] {
     return this.boards;
+  }
+
+  create(title: string, description: string): Board {
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardsStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 }
