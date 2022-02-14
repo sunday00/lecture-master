ecaf40c2baca86b79ce3a6921c4de1a89151685b
sunday00 <kind65505256@gmail.com>
2022-02-15 00:28:42 +0900

    apply store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+import { fetchNews } from '@a/index';
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -5,10 +6,26 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    news: {},
+    asks: {},
+    jobs: {},
   },
   mutations: {
+    SET_NEWS(state, data) {
+      state.news = data;
+    },
   },
   actions: {
+    FETCH_NEWS(ctx) {
+      fetchNews(1).then((res) => {
+        ctx.commit('SET_NEWS', res.data);
+      }).catch((err) => {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      });
+    },
+  },
+  getters: {
   },
   modules: {
   },
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <li v-for="item in news" :key="item.id">
+    <li v-for="item in this.$store.state.news" :key="item.id">
       <router-link :to="`/news/${ item.id }`">
         {{ item.title }}
       </router-link>
@@ -9,21 +9,13 @@
 </template>
 
 <script>
-import { fetchNews } from '@a/index';
-
 export default {
   data() {
     return {
-      news: [],
     };
   },
   created() {
-    fetchNews().then((res) => {
-      this.news = res.data;
-    }).catch((err) => {
-      // eslint-disable-next-line no-console
-      console.log(err);
-    });
+    this.$store.dispatch('FETCH_NEWS');
   },
 };
 </script>
