22c4c56dbfb40d4e912f5e28f5cf282c77fc77f9
sunday00 <kind65505256@gmail.com>
2022-05-15 19:14:40 +0900

    :tada: react-study setting
    react-study setting

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React!!</h1>
+      <h2>This is made with Vite</h2>
+    </div>
+  );
+}
+
+export default App;
