4c53abc5c058b1067d52226a3bd8f87dd7196159
sunday00 <kind65505256@gmail.com>
2022-06-19 20:04:22 +0900

    :lipstick: style
    style

---
+++ p4-style-1/src/components/StyledElement.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MyComponent = styled.div`
+  font-size: 2rem;
+`;
+
+const MyInput = styled('input')`
+  background: blue;
+`;
