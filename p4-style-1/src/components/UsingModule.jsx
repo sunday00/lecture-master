78151abf4b3d0ad67ba22feffeeeb2a2b8264458
sunday00 <kind65505256@gmail.com>
2022-06-19 18:51:00 +0900

    :heavy_plus_sign: classNames.js
    classNames.js

--- p4-style-1/src/components/UsingModule.jsx
+++ p4-style-1/src/components/UsingModule.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import styles from '../assets/scss/Module.module.scss';
+
+const cx = classNames.bind(styles);
 
 const UsingModule = () => {
   return (
-    <div className={styles.wrapper}>
+    // <div className={styles.wrapper}>
+    <div className={cx('wrapper')}>
       Hello, I'm <span className="something">Module...</span>
     </div>
   );
