81dd1f144ec238676d04947a54350a06bf80ce29
sunday00 <kind65505256@gmail.com>
2022-07-17 20:50:40 +0900

    :art: 1
    1

--- p11-bigdata/src/views/Home.jsx
+++ p11-bigdata/src/views/Home.jsx
@@ -1,8 +1,8 @@
-import { useCallback, useRef, useState } from 'react';
+import {useCallback, useRef, useState} from 'react';
 
 const Home = () => {
   const nextId = useRef(1);
-  const [form, setForm] = useState({ name: '', username: '' });
+  const [form, setForm] = useState({name: '', username: ''});
   const [data, setData] = useState({
     array: [],
     uselessValue: null,
@@ -10,7 +10,7 @@ const Home = () => {
 
   const onChange = useCallback(
     (e) => {
-      const { name, value } = e.target;
+      const {name, value} = e.target;
       setForm({
         ...form,
         [name]: [value],
@@ -32,21 +32,34 @@ const Home = () => {
       array: data.array.concat(info),
     });
 
-    setForm({ name: '', username: '' });
+    setForm({name: '', username: ''});
 
     nextId.current++;
-  });
+  }, [data, form.name, form.username]);
 
   const onRemove = useCallback((id) => {
     setData({
       ...data,
       array: data.array.filter((info) => info.id !== id),
     });
-  });
+  }, [data]);
 
   return (
     <div className="home-wrap">
-      <section className="hero min-h-screen"></section>
+      <form onSubmit={onSubmit}>
+        <input name="username" placeholder="id" className="input input-bordered input-primary w-full max-w-xs" value={form.username} onChange={onChange} />
+        <input name="name" placeholder="name" className="input input-bordered input-primary w-full max-w-xs" value={form.name} onChange={onChange} />
+        <input type="submit" className="btn" value="send" />
+      </form>
+      <div>
+        <ul>
+          {data.array.map(info => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
