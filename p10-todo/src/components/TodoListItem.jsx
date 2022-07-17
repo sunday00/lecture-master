13b7e605147f34c18b5bbd454af6c8ed0bed7156
sunday00 <kind65505256@gmail.com>
2022-07-17 19:48:28 +0900

    :bug: resolve vite react virtualized
    resolve vite react virtualized

--- p10-todo/src/components/TodoListItem.jsx
+++ p10-todo/src/components/TodoListItem.jsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import {
   MdCheckBoxOutlineBlank,
   MdCheckBox,
@@ -6,11 +7,11 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
+    <div className="TodoListItem-virtualized" style={style}>
       <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
@@ -22,4 +23,4 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
   );
 };
 
-export default TodoListItem;
+export default memo(TodoListItem);
--- p10-todo/vite.config.js
+++ p10-todo/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+const resolveFixup = {
+  name: 'resolve-fixup',
+  setup(build) {
+    build.onResolve({ filter: /react-virtualized/ }, async (args) => {
+      return {
+        path: path.resolve(
+          './node_modules/react-virtualized/dist/umd/react-virtualized.js',
+        ),
+      };
+    });
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [resolveFixup],
+    },
+  },
+});
