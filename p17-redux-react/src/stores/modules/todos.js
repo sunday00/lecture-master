012d14497aaf67e01606d7aa051a268cd3549c5e
sunday00 <kind65505256@gmail.com>
2022-08-21 20:35:43 +0900

    :construction: recoil
    recoil

--- p17-redux-react/src/stores/modules/todos.js
+++ p17-redux-react/src/stores/modules/todos.js
@@ -7,10 +7,10 @@ export const change_input = input => ({type: CHANGE_INPUT, input})
 
 let id = 3
 export const insert = text => ({type: INSERT, todo: {
-    id: id++,
-    text,
-    done: false,
-  }})
+  id: id++,
+  text,
+  done: false,
+}})
 
 export const toggle = id => ({ type: TOGGLE, id })
 
