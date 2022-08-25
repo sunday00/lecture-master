5d77ce1e30e6ac03af56a72441658e620fb3203a
sunday00 <kind65505256@gmail.com>
2022-08-26 01:03:32 +0900

    :construction: user validation add preg name
    user validation add preg name

--- src/auth/dtos/create-user.dto.ts
+++ src/auth/dtos/create-user.dto.ts
@@ -1,7 +1,10 @@
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { IsNotEmpty, Matches, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @Matches(/^([a-zA-Z]|\d)*$/, {
+    message: 'only can be eng and num',
+  })
   username: string;
 
   @IsNotEmpty()
