0d39471d42597d8f69a7c7782007759610a4f8eb
sunday00 <kind65505256@gmail.com>
2022-12-11 18:29:56 +0900

    :construction: re recoil
    recoil

---
+++ p23-recoil-official-tutorial/src/Toolbar.tsx
@@ -0,0 +1,27 @@
+import {Icon, IconButton, VStack} from '@chakra-ui/react'
+import {useContext} from 'react'
+import {Square} from 'react-feather'
+import {ElementsContext} from './Canvas'
+
+export const Toolbar = () => {
+    const {addElement} = useContext(ElementsContext)
+
+    return (
+      <VStack
+        position="absolute"
+        top="20px"
+        left="20px"
+        backgroundColor="white"
+        padding={2}
+        boxShadow="md"
+        borderRadius="md"
+        spacing={2}
+      >
+          <IconButton
+            onClick={addElement}
+            aria-label="Add rectangle"
+            icon={<Icon style={{width: 24, height: 24}} as={Square} />}
+          />
+      </VStack>
+    )
+}
---
+++ p23-recoil-official-tutorial/src/components/Rectangle/Rectangle.tsx
@@ -0,0 +1,43 @@
+import {useContext} from 'react'
+import {ElementsContext, SelectedElementContext} from '../../Canvas'
+import {Drag} from '../Drag'
+import {RectangleContainer} from './RectangleContainer'
+import {RectangleInner} from './RectangleInner'
+
+export type ElementStyle = {
+  position: {top: number; left: number}
+  size: {width: number; height: number}
+}
+
+export type Element = {style: ElementStyle}
+
+export const Rectangle = ({element, index}: {element: Element; index: number}) => {
+  const {selectedElement, setSelectedElement} = useContext(SelectedElementContext)
+  const {setElement} = useContext(ElementsContext)
+
+  return (
+    <Drag
+      position={element.style.position}
+      onDrag={(position) => {
+        setElement(index, {
+          style: {
+            ...element.style,
+            position,
+          },
+        })
+      }}
+    >
+      <div>
+        <RectangleContainer
+          position={element.style.position}
+          size={element.style.size}
+          onSelect={() => {
+            setSelectedElement(index)
+          }}
+        >
+          <RectangleInner selected={index === selectedElement} />
+        </RectangleContainer>
+      </div>
+    </Drag>
+  )
+}
