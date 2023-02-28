94e1db5906fb7fc69f46c4227d9823f8afe4f75d
sunday00tdi <102128587+sunday00tdi@users.noreply.github.com>
2023-03-01 04:12:54 +0900

    :sparkles: slice
    slice

--- 012-slice/pkg/basic.go
+++ 012-slice/pkg/basic.go
@@ -3,6 +3,7 @@ package pkg
 import (
 	"fmt"
 	"github.com/sunday00/go-console"
+	"sort"
 )
 
 func (p Pkg) Basic() {
@@ -97,6 +98,107 @@ func changeSlice(slice []int) {
 
 func changeArrayReal(arr *[5]int) {
 	arr[2] = 100
+}
+
+func (p Pkg) Slicing() {
+	arr := [6]int{1, 2, 3, 4, 5, 6}
+	sl := arr[2:4] // 이때 4은 4인덱스가 아니라, 4번째 슬라이싱 포인트를 나타내므로, index4을 포함하지 않는 이전의 수들을 의미.
+	// 0   1   *   3   *   5   6  // pointer
+	// | 0 | 1 | 2 | 3 | 4 | 5 |  // index
+	// | 1 | 2 | 3 | 4 | 5 | 6 |  // values
+
+	fmt.Printf("%T %v\n", sl, sl) // -> [3, 4]
+
+	arr[3] = 40
+
+	fmt.Println(arr, sl) // slice from arr also ref original arr, so modify arr element that includes slice index, slice value also update.
+
+	sl = append(sl, 50)
+
+	fmt.Println(arr, sl) // even more, changing slice, array also updated.
+}
+
+func (p Pkg) ArrToSlice() {
+	arr := [6]int{1, 2, 3, 4, 5, 6}
+	sl := arr[:]
+
+	fmt.Println(sl)
+}
+
+func (p Pkg) SliceSlice() {
+	arr := [100]int{1, 2, 3, 4, 5, 6, 7, 99: 100}
+	sl1 := arr[0:10]
+	sl2 := sl1[4:9:10] // setting cap last number
+
+	fmt.Println(sl1, sl2)
+}
+
+func (p Pkg) Clone() {
+	sl1 := []int{1, 2, 3, 4, 5, 6}
+
+	sl2 := append([]int{}, sl1...)
+
+	sl3 := make([]int, len(sl1))
+	copy(sl3, sl1)
+
+	fmt.Println(sl1, sl2, sl3)
+
+	sl1[2] = 100
+
+	fmt.Println(sl1, sl2, sl3)
+}
+
+func (p Pkg) Delete() {
+	sl1 := []int{1, 2, 3, 4, 5, 6}
+	wantDeleteIdx := 2
+
+	sl2 := append(sl1[:wantDeleteIdx], sl1[wantDeleteIdx+1:]...)
+
+	fmt.Println(sl1, sl2)
+}
+
+func (p Pkg) Insert() {
+	sl1 := []int{1, 2, 3, 4, 5, 6}
+	wantInsertIdx := 2
+	wantInsertValue := 40
+
+	sl2 := append(sl1[:wantInsertIdx], append([]int{wantInsertValue}, sl1[wantInsertIdx:]...)...)
+
+	sl3 := append(sl1, 0)
+	copy(sl3[wantInsertIdx+1:], sl3[wantInsertIdx:])
+	sl3[wantInsertIdx] = wantInsertValue
+
+	fmt.Println(sl1, sl2, sl3)
+}
+
+type Student struct {
+	Name string
+	Age  int
+}
+
+type Students []Student
+
+func (s Students) Len() int           { return len(s) }
+func (s Students) Less(i, j int) bool { return s[i].Age < s[j].Age }
+func (s Students) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+func (p Pkg) Sorting() {
+	sl1 := []int{6, 8, 34, 6, 8, 23, 5, 78, 9, 5}
+
+	sort.Ints(sl1)
+
+	fmt.Println(sl1)
+
+	stu := Students{
+		{"Hayato", 18},
+		{"Schumach", 20},
+		{"Randol", 18},
+		{"Kaga", 20},
+	}
+
+	sort.Sort(stu)
+
+	fmt.Printf("%v\n", stu)
 }
 
 func (p Pkg) template() {
