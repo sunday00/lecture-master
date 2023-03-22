1bf596ed264f4d1f7271c1c7a5b45fb989fda142
sunday00tdi <102128587+sunday00tdi@users.noreply.github.com>
2023-03-23 00:30:23 +0900

    :wrench: base config
    base config

---
+++ src/apps/configs/swagger.config.ts
@@ -0,0 +1,34 @@
+import { INestApplication } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+export const swaggerConfig = (
+  path: string,
+  version: string,
+  app: INestApplication,
+) => {
+  SwaggerModule.setup(
+    path,
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Ytb-advance')
+        .setDescription('advance technique for nest')
+        .setVersion(version)
+        // .addTag('cats')
+        // .addSecurity('apiKey', {
+        //   type: 'apiKey',
+        //   in: 'header',
+        //   name: 'Api-Key',
+        //   description: '9XqF7CsiOa8nWJb60sHUuA2Mcguts2OOSJ+WSlOvHpg=',
+        // })
+        // .addSecurityRequirements('apiKey')
+        // .addBearerAuth(
+        //   { type: 'apiKey', in: 'header', name: 'authorization' },
+        //   'authorization',
+        // )
+        // .addSecurityRequirements('authorization')
+        .build(),
+    ),
+  )
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe, VersioningType } from '@nestjs/common'
+import { swaggerConfig } from './apps/configs/swagger.config'
 
 const HARDCODE_CONFIGS = {
   port: 3000,
+  version: '1',
 }
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: HARDCODE_CONFIGS.version,
+  })
+
+  swaggerConfig('swagger', HARDCODE_CONFIGS.version, app)
+
+  app.useGlobalPipes(new ValidationPipe())
+
   await app.listen(HARDCODE_CONFIGS.port)
 }
-bootstrap().then(() =>
-  console.log(`Server is running on : ${HARDCODE_CONFIGS.port}`),
-)
+bootstrap()
+  .then(() => console.log(`Server is running on : ${HARDCODE_CONFIGS.port}`))
+  .catch((e) => console.error(e))
