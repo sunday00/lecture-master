99f62b02b0e47579367c5790f25233d5b27e45e9
sunday00tdi <102128587+sunday00tdi@users.noreply.github.com>
2023-07-15 14:54:22 +0900

    :construction: joystick

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,15 +1,12 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/palette.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-import 'components/LifeBar.dart';
+import 'package:hello_world/components/joystick_player.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,150 +17,27 @@ void main() {
   ));
 }
 
-class Square extends PositionComponent {
-  Vector2 velocity = Vector2(0, 0).normalized() * 0;
-  double squareSize;
-  double rotationSpeed;
-  Paint color = BasicPalette.white.paint()
-    ..style = PaintingStyle.stroke
-    ..strokeWidth = 0;
-  late LifeBar lifeBar;
-
-  Square({
-    required Vector2 position,
-    required this.velocity,
-    required this.squareSize,
-    required this.rotationSpeed,
-    required this.color,
-  }) : super(position: position);
-
-  createLifeBar() {
-    lifeBar = LifeBar.initData(size,
-        size: Vector2(size.x - 10, 5), placement: LifeBarPlacement.center);
-    add(lifeBar);
-  }
+class MyApp extends FlameGame with HasDraggablesBridge {
+  late final JoystickPlayer player;
+  late final JoystickComponent joystick;
 
   @override
   FutureOr<void> onLoad() {
     super.onLoad();
 
-    size.setValues(squareSize, squareSize);
+    final knobPaint = BasicPalette.green.withAlpha(200).paint();
+    final backgroundPaint = BasicPalette.green.withAlpha(100).paint();
 
-    anchor = Anchor.center;
+    joystick = JoystickComponent(
+      knob: CircleComponent(radius: 15, paint: knobPaint),
+      background: CircleComponent(radius: 50, paint: backgroundPaint),
+      // screen position margin
+      margin: const EdgeInsets.only(left: 20, bottom: 20),
+    );
 
-    createLifeBar();
-  }
+    player = JoystickPlayer(joystick);
 
-  @override
-  void update(double dt) {
-    super.update(dt);
-
-    position += velocity * dt;
-
-    angle = (angle - (dt * rotationSpeed)) % (2 * pi);
-  }
-
-  @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-    canvas.drawRect(size.toRect(), color);
-  }
-
-  processHit() {
-    lifeBar.decrementCurrentLifeBy(10);
-  }
-}
-
-class MyApp extends FlameGame with TapDetector, DoubleTapDetector {
-  bool running = true;
-
-  @override
-  bool get debugMode {
-    super.debugMode;
-    return kDebugMode;
-  }
-
-  final TextPaint textPaint = TextPaint(
-    style: const TextStyle(
-      fontSize: 36.0,
-      fontFamily: 'Awesome Font',
-    ),
-  );
-
-  loadFps() {
-    if (!kDebugMode) return;
-    // info: opposite: kReleaseMode
-
-    add(FpsTextComponent(
-      textRenderer: TextPaint(
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 35,
-        ),
-      ),
-      position: Vector2(0, 0),
-    )..anchor = Anchor.topLeft);
-  }
-
-  @override
-  FutureOr<void> onLoad() {
-    loadFps();
-
-    return super.onLoad();
-  }
-
-  @override
-  void update(double dt) {
-    super.update(dt);
-  }
-
-  @override
-  void render(Canvas canvas) {
-    canvas.drawPaint(Paint()..color = Colors.red.shade200);
-
-    textPaint.render(
-        canvas, 'active: ${children.length}', Vector2(canvasSize.x, 0),
-        anchor: Anchor.topRight);
-
-    super.render(canvas);
-  }
-
-  @override
-  void onTapUp(TapUpInfo info) {
-    // print('TAP: ${info.eventPosition.game.x}, ${info.eventPosition.game.y}');
-    final touchPoint = info.eventPosition.game;
-
-    final handled = children.any((element) {
-      if (element is Square && element.containsPoint(touchPoint)) {
-        element.velocity.negate();
-        element.processHit();
-        return true;
-      }
-      return false;
-    });
-
-    if (!handled) {
-      add(
-        Square(
-            position: touchPoint,
-            squareSize: 45.0,
-            velocity: Vector2(0, 1).normalized() * 50,
-            rotationSpeed: 0.3,
-            color: (BasicPalette.red.paint()
-              ..style = PaintingStyle.stroke
-              ..strokeWidth = 2)),
-      );
-    }
-  }
-
-  @override
-  void onDoubleTap() {
-    if (running) {
-      pauseEngine();
-    } else {
-      resumeEngine();
-    }
-
-    running = !running;
+    add(player);
+    add(joystick);
   }
 }
