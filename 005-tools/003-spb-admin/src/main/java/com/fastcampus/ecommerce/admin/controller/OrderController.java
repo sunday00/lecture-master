1e1ddb388a984acda19262e4854dd56b13cf7b08
sunday00 <kind65505256@gmail.com>
2023-10-11 15:30:31 +0900

    :sparkles: Admin pannel

---
+++ 005-tools/003-spb-admin/src/main/java/com/fastcampus/ecommerce/admin/controller/OrderController.java
@@ -0,0 +1,39 @@
+package com.fastcampus.ecommerce.admin.controller;
+
+import com.fastcampus.ecommerce.admin.domain.order.OrderDetailView;
+import com.fastcampus.ecommerce.admin.exception.NotFoundOrderException;
+import com.fastcampus.ecommerce.admin.service.OrderService;
+import com.fastcampus.ecommerce.admin.service.dto.OrderDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@AllArgsConstructor
+public class OrderController {
+
+    public static final String MENU_KEY = "orders";
+    private final OrderService orderService;
+
+    @GetMapping({"/orders/", "/orders"})
+    public String index(Model model) {
+        List<OrderDetailView> orderDetailViews = orderService.findAllOrderDetailView();
+        model.addAttribute("orders", orderDetailViews);
+        model.addAttribute("menuId", MENU_KEY);
+        return "/orders/orders";
+    }
+
+    @GetMapping("/orders/order-detail")
+    public String detail(@RequestParam Long orderId, Model model) {
+        Optional<OrderDTO> optionalOrderDTO = orderService.findById(orderId);
+        OrderDTO orderDTO = optionalOrderDTO.orElseThrow(() -> new NotFoundOrderException("Not found order info"));
+        model.addAttribute("order", orderDTO);
+        model.addAttribute("menuId", MENU_KEY);
+        return "/orders/order-detail";
+    }
+}
