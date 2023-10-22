e94259b261e1daceb1171f4f2608752cc10bcc63
sunday00 <kind65505256@gmail.com>
2023-10-22 22:35:50 +0900

    ✨ standard

--- academind/002-observer/src/App.tsx
+++ academind/002-observer/src/App.tsx
@@ -1,10 +1,24 @@
-function App() {
+import {useEffect, useRef} from "react";
+import {fromEvent} from "rxjs";
 
+function App() {
+  const btnRef = useRef(null)
+
+  useEffect(() => {
+    const src$ = fromEvent(btnRef.current!, 'click')
+    const piped = src$.pipe()
+
+    const subscription = piped.subscribe((coordinate) => {
+      console.log(coordinate)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
 
   return (
     <>
       <main>
-
+        <button ref={btnRef}>Click</button>
       </main>
     </>
   )
