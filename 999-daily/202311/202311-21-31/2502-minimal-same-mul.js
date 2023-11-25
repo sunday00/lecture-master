7fd7ff72ad09ffdaf4eed62454d20559ace94a65
sunday00 <kind65505256@gmail.com>
2023-11-25 15:49:33 +0900

    :sparkles: Minimal same mul

---
+++ 999-daily/202311/202311-21-31/2502-minimal-same-mul.js
@@ -0,0 +1,30 @@
+let fs = require('fs')
+if(fs.existsSync('./example.txt')) {
+    input = fs.readFileSync('./example.txt').toString().trim()
+} else {
+    input = fs.readFileSync('/dev/stdin').toString().trim()
+}
+
+let [n, ...data] = input.split('\n')
+// @info: logic start
+// @@info: prepare for using
+
+for(const d of data) {
+    let [a,  b] = d.split(' ').map(Number).sort((a,b) => a-b)
+
+    let currentATimes = 1
+    let currentBTimes = 0
+
+    let subAns = -1
+
+    while(currentATimes * a !== currentBTimes * b) {
+        currentBTimes++
+        let start = currentATimes
+        for(let i=start;a*i <= b*currentBTimes; i++) {
+            if(a*i === b*currentBTimes) subAns = a * i
+            currentATimes = i
+        }
+    }
+
+    console.log(subAns)
+}
